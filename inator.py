a39e5e3f6cf77d6c975f8da7b76330fb87c928ff
saish <saishreddy05@gmail.com>
2021-01-05 11:29:07 +0530

    fixed "same hour" bug, modified naming to follow pip-8 guidelines

--- inator.py
+++ inator.py
@@ -4,6 +4,7 @@ import datetime
 from selenium.common.exceptions import NoSuchElementException
 
 hour = int(datetime.datetime.now().strftime("%H"))
+minute = int(datetime.datetime.now().strftime("%M"))
 
 
 class Automate:
@@ -17,23 +18,25 @@ class Automate:
             print("Getting credentials from saved file...")
             with open("C:\\Users\\Public\\credentials.txt", "r") as f:
                 creds = f.readline()
-                UserName = creds[:8]
-                PassWord = creds[8:]
+                saved_username = creds[:8]
+                saved_password = creds[8:]
             f.close()
+
         except FileNotFoundError:
             print("\n Its your first time using this console app, please enter your credentials below (needed only "
                   "once) they will be stored in Public user folder")
             with open("C:\\Users\\Public\\credentials.txt", "w") as f:
-                UserName = input("Username: ")
-                PassWord = input("Password: ")
-                f.write(UserName)
-                f.write(PassWord)
+                new_username = input("Username: ")
+                new_password = input("Password: ")
+                f.write(new_username)
+                f.write(new_password)
+                saved_username, saved_password = new_username, new_password
             f.close()
 
         username = self.driver.find_element_by_id("username")
-        username.send_keys(UserName)  # your id
+        username.send_keys(saved_username)  # your id
         password = self.driver.find_element_by_id("password")
-        password.send_keys(PassWord)  # your password
+        password.send_keys(saved_password)  # your password
         login_button = self.driver.find_element_by_id("loginbtn")
         login_button.click()
 
@@ -45,19 +48,22 @@ class Automate:
                     self.classes[self.driver.find_element_by_xpath(
                         path + "div[2]/header/h2").text] = \
                         [self.driver.find_element_by_xpath(path + "div[3]/a"),
-                         int((self.driver.find_element_by_xpath(
-                             "/html/body/div[2]/div/section/div/div/div/div[1]/div[2]/header/span/div/span[1]").text[-7]
-                             ).strip())]
+                         self.driver.find_element_by_xpath(
+                             "/html/body/div[2]/div/section/div/div/div/div[1]/div[2]/header/span/div/span[1]"
+                             "").text[-8:-3].strip().split(":")]
 
             except NoSuchElementException:
                 pass
 
     def join_most_recent_class(self):
         if len(self.classes.keys()) > 1:
-            priority_list = sorted(self.classes.items(), key=lambda x: hour - x[1][1])
+            priority_list = sorted(self.classes.items(),
+                                   key=lambda x: (hour*60 + minute) - (int(x[1][1][0])*60 + int(x[1][1][1])))
             priority_list[0][1][0].click()
             return
+
         if len(self.classes.keys()) == 1:
             list(self.classes.items())[0][1][0].click()
             return
+
         print("No classes found, please check manually otherwise just sleep ZZZZZZZZZZZZZZZZZZ")
