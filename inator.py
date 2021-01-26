e1d71b85237e6e5e10ad9333a0d4a9185e04eb2e
saishreddyk <saishreddy05@gmail.com>
2021-01-26 11:16:28 +0530

    Updated files, performance improvements, packages optimized, Made more user friendly and bug fixes.

--- inator.py
+++ inator.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
-import selenium
 import datetime
 from selenium.common.exceptions import NoSuchElementException
+from time import sleep
 
 hour = int(datetime.datetime.now().strftime("%H"))
 minute = int(datetime.datetime.now().strftime("%M"))
@@ -9,7 +9,7 @@ minute = int(datetime.datetime.now().strftime("%M"))
 
 class Automate:
     def __init__(self):
-        self.driver = selenium.webdriver.Chrome("chromedriver.exe")
+        self.driver = webdriver.Chrome("chromedriver.exe")
         self.driver.get("http://ngitonline.com")
         self.classes = {}
 
@@ -41,37 +41,43 @@ class Automate:
         login_button.click()
 
     def find_classes(self):
+        def collector():
+            self.classes[self.driver.find_element_by_xpath(
+                path + "div[2]/header/h2").text] = \
+                [self.driver.find_element_by_xpath(path + "div[3]/a"),
+                 self.driver.find_element_by_xpath(
+                     path + "div[2]/header/span/div/span[1]"
+                            "").text[-8:-3].strip().split(":")]
+
         for i in range(1, 30):
+            path = "/html/body/div[2]/div/section/div/div/div/div[{}]/".format(i)
             try:
-                path = "/html/body/div[2]/div/section/div/div/div/div[{}]/".format(i)
                 if self.driver.find_element_by_xpath(path + "div[3]/a/span").text == "JOIN":
-                    self.classes[self.driver.find_element_by_xpath(
-                        path + "div[2]/header/h2").text] = \
-                        [self.driver.find_element_by_xpath(path + "div[3]/a"),
-                         self.driver.find_element_by_xpath(
-                             path + "div[2]/header/span/div/span[1]"
-                             "").text[-8:-3].strip().split(":")]
-
-                elif self.driver.find_element_by_xpath(path + "div[3]/form/a/span").text == "JOIN":
-                    self.classes[self.driver.find_element_by_xpath(
-                        path + "div[2]/header/h2").text] = \
-                        [self.driver.find_element_by_xpath(path + "div[3]/form/a"),
-                         self.driver.find_element_by_xpath(
-                             path + "div[2]/header/span/div/span[1]"
-                             "").text[-8:-3].strip().split(":")]
-
+                    collector()
             except NoSuchElementException:
-                pass
+                try:
+                    if self.driver.find_element_by_xpath(path + "div[3]/form/a/span").text == "JOIN":
+                        collector()
+                except NoSuchElementException:
+                    pass
 
     def join_most_recent_class(self):
         if len(self.classes.keys()) > 1:
             priority_list = sorted(self.classes.items(),
-                                   key=lambda x: (hour*60 + minute) - (int(x[1][1][0])*60 + int(x[1][1][1])))
+                                   key=lambda x: (hour * 60 + minute) - (int(x[1][1][0]) * 60 + int(x[1][1][1])))
             priority_list[0][1][0].click()
+            print("Joined a class, now dont doze off!!")
+            self.driver.quit()
+            sleep(10)
             return
 
-        if len(self.classes.keys()) == 1:
+        elif len(self.classes.keys()) == 1:
             list(self.classes.items())[0][1][0].click()
+            print("Joined a class, now dont doze off")
+            self.driver.quit()
+            sleep(10)
             return
-
-        print("No classes found, please check manually otherwise just sleep ZZZZZZZZZZZZZZZZZZ")
+        else:
+            print("No classes found, please check manually otherwise just sleep ZZZZZZZZZZZZZZZZZZ")
+            self.driver.quit()
+            return
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import inator
-print("Thank you for using ClassJoinInator, \n\
-please give your feedback at https://docs.google.com/forms/d/e/1FAIpQLSdPCX6swB7Xsg6HsimZZGxsI8Ink1Lj3VqZ4pky3TzCD6yvew/viewform?usp=sf_link \n\n\n")
-
+print("Thank you for using ClassJoinInator")
 print("Initiated automation software...")
 a1 = inator.Automate()
 print("Trying to login...")
